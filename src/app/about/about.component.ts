c6215a29f5230599d29b6783a573744701722330
nayan <nayan.drakshapally@gmail.com>
2018-03-03 22:33:58 +0530

    updated including params

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Country } from '../shared/country';
+import { COUNTRIES } from '../shared/countries';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  country: Country[];
+  private sub: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.country = params['country']; // (+) converts string 'id' to a number
+       console.log("testtttttt2222222222",this.country);
+
+       // In a real app: dispatch action to load the details here.
+    });
   }
 
 }
